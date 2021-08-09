06aae63e794c15929414366227d403b22b1e5ad5
Goutham-AR <argouthu8400@gmail.com>
2021-08-10 01:43:46 +0530

    some refactoring

--- GEngine/src/events/KeyEvent.hh
+++ GEngine/src/events/KeyEvent.hh
@@ -11,8 +11,8 @@ class GE_PUBLIC KeyEvent : public Event
 {
 
 public:
-    int getCategoryFlags() const override { return EC_KeyBoard | EC_Input; }
-    int getKeyCode() const { return m_keyCode; }
+    [[nodiscard]] int getCategoryFlags() const override { return EC_KeyBoard | EC_Input; }
+    [[nodiscard]] int getKeyCode() const { return m_keyCode; }
 
 protected:
     explicit KeyEvent(int keyCode)
@@ -32,9 +32,9 @@ public:
     {
     }
 
-    int getCount() const { return m_count; }
+    [[nodiscard]] int getCount() const { return m_count; }
 
-    std::string toString() const override
+    [[nodiscard]] std::string toString() const override
     {
         std::stringstream sstream;
         sstream << "KeyPressedEvent: " << m_keyCode << " (" << m_count << " repeats)";
@@ -42,8 +42,8 @@ public:
     }
 
     static EventType getStaticType() { return EventType::KeyPressed; }
-    EventType getEventType() const override { return getStaticType(); }
-    const char* getName() const override { return TO_STRING(KeyPressed); }
+    [[nodiscard]] EventType getEventType() const override { return getStaticType(); }
+    [[nodiscard]] const char* getName() const override { return TO_STRING(KeyPressed); }
 
 private:
     int m_count;
@@ -57,7 +57,7 @@ public:
     {
     }
 
-    std::string toString() const override
+    [[nodiscard]] std::string toString() const override
     {
         std::stringstream sstream;
         sstream << "KeyReleasedEvent: " << m_keyCode;
@@ -65,8 +65,8 @@ public:
     }
 
     static EventType getStaticType() { return EventType::KeyReleased; }
-    EventType getEventType() const override { return getStaticType(); }
-    const char* getName() const override { return TO_STRING(KeyReleased); }
+    [[nodiscard]] EventType getEventType() const override { return getStaticType(); }
+    [[nodiscard]] const char* getName() const override { return TO_STRING(KeyReleased); }
 };
 
 class GE_PUBLIC KeyTypedEvent : public KeyEvent
@@ -77,7 +77,7 @@ public:
     {
     }
 
-    std::string toString() const override
+    [[nodiscard]] std::string toString() const override
     {
         std::stringstream sstream;
         sstream << "KeyTypedEvent: " << m_keyCode;
@@ -85,8 +85,8 @@ public:
     }
 
     static EventType getStaticType() { return EventType::KeyTyped; }
-    EventType getEventType() const override { return getStaticType(); }
-    const char* getName() const override { return TO_STRING(KeyTyped); }
+    [[nodiscard]] EventType getEventType() const override { return getStaticType(); }
+    [[nodiscard]] const char* getName() const override { return TO_STRING(KeyTyped); }
 };
 
 }
