83631f72dca5500c75056d3705bb2034097d39e9
Goutham-AR <argouthu8400@gmail.com>
2021-08-18 17:08:10 +0530

    renamed from Shader to IShader, also create returns a shared_ptr

--- GEngine/src/graphics/Shader.cc
+++ GEngine/src/graphics/Shader.cc
@@ -1,15 +1,30 @@
 #include "Shader.hh"
-
+#include <utils/Logger.hh>
 #include "GL/GLShader.hh"
 
 namespace GE
 {
-Shader* Shader::create(std::string_view vertexPath, std::string_view fragmentPath)
+Sptr<IShader> IShader::create(std::string_view vertexPath, std::string_view fragmentPath)
 {
-    return new GLShader{vertexPath, fragmentPath};
+    return Sptr<IShader>{new GLShader{vertexPath, fragmentPath}};
 }
 
+void ShaderLibrary::addShader(const Sptr<IShader>& shader)
+{
+    auto& name = shader->getShaderFileName();
+    GE_ASSERT(m_shaders.find(name) == m_shaders.end(), "Shader already Exist");
+    m_shaders.insert({shader->getShaderFileName(), shader});
+}
+Sptr<IShader> ShaderLibrary::getShader(const std::string& name)
+{
+    GE_ASSERT(m_shaders.find(name) != m_shaders.end(), "Shader does not Exist");
+    return m_shaders.at(name);
+}
 
-
+void ShaderLibrary::load(std::string_view vertexPath, std::string_view fragmentPath)
+{
+    auto shader = IShader::create(vertexPath, fragmentPath);
+    addShader(shader);
+}
 
 }
--- GEngine/src/graphics/Shader.hh
+++ GEngine/src/graphics/Shader.hh
@@ -3,19 +3,32 @@
 #include <common.hh>
 #include <string_view>
 #include <glm/glm.hpp>
+#include <unordered_map>
 
 namespace GE
 {
 
-class GE_PUBLIC Shader
+class GE_PUBLIC IShader
 {
 public:
-    virtual ~Shader() = default;
+    virtual ~IShader() = default;
 
     virtual void bind() const = 0;
     virtual void unbind() const = 0;
+    [[nodiscard]] virtual const std::string& getShaderFileName() const = 0;
 
-    static Shader* create(std::string_view vertexPath, std::string_view fragmentPath);
+    static Sptr<IShader> create(std::string_view vertexPath, std::string_view fragmentPath);
+};
+
+class GE_PUBLIC ShaderLibrary
+{
+public:
+    void addShader(const Sptr<IShader>& shader);
+    Sptr<IShader> getShader(const std::string& name);
+    void load(std::string_view vertexPath, std::string_view fragmentPath);
+
+private:
+    std::unordered_map<std::string, Sptr<IShader>> m_shaders;
 };
 
 }
